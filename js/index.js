488745190488dfed20c8861537c97deb93c40d36
cnabilgit <46906074+cnabilgit@users.noreply.github.com>
2021-03-14 22:21:01 +0100

    ok

--- js/index.js
+++ js/index.js
@@ -2,4 +2,15 @@ window.addEventListener("load", () => {
   setTimeout(() => {
     document.getElementById("loading").classList.add("d-none");
   }, 1000);
+
+  // setInterval(() => {
+  //   document.querySelectorAll(".screenshot").forEach((el, index) => {
+  //     el.classList.add("opacity-4");
+  //     if (index == 3) {
+  //       el.classList.remove("opacity-4");
+  //       el.classList.add("opacity-5");
+  //       console.log(index);
+  //     }
+  //   });
+  // }, 0);
 });
